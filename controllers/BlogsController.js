a69d7fc39ab9fef9332c781cc0f584a1ae3244ce
yeetesh <yeetesh1997@gmail.com>
2018-10-20 11:55:11 +0530

    small changes

--- controllers/BlogsController.js
+++ controllers/BlogsController.js
@@ -9,11 +9,11 @@ module.exports = {
         Blog.create(data, function(err,doc) {
             var response = {}
             if(err){
-                response['message'] = 'Could not create blog'
+                response['message'] = 'could not create blog'
                 res.status(404).send(response)
             }
             else {
-                response['message'] = 'Succesfully created'
+                response['message'] = 'succesfully created'
                 res.json(response)
             }
         })
@@ -22,7 +22,7 @@ module.exports = {
         Blog.find(function(err,doc) {
             var response = {}
             if (err) {
-                res['message'] = 'Blogs not found'
+                res['message'] = 'blogs not found'
                 res.status(404).json(response)
             }
             else {
@@ -35,12 +35,25 @@ module.exports = {
         Blog.find({'author' : req.params.username }, function(err,doc) {
             var response = {}
             if (err) {
-                res['message'] = 'Blogs not found'
+                res['message'] = 'blogs not found'
                 res.status(404).json(response)
             }
             else {
                 response['blogs'] = doc
                 res.status(200).json(response)
+            }
+        })
+    },
+    getBlog(req,res) {
+        Blog.findById(req.params.id, function (err,doc) {
+            if(err) {
+                res.status(404).json({'message' : 'blog not found'})
+            }
+            else if(doc == null) {
+                res.status(404).json({'message' : 'blog not found'})
+            }
+            else {
+                res.status(200).json(doc)
             }
         })
     }
